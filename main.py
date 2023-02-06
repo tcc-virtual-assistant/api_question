6311079eebbbd6e3b0556d437238a58bbcd94ee4
joao-montanari <joao.unipbr@gmail.com>
2023-02-06 11:07:33 -0300

    introducao

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    descricao: str
+    valor: float
 
 app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'Hello' : 'world'}
+    return {'Hello' : 'world 2'}
 
 @app.get('/items/{item_id}')
-def read_item(item_id: int, q: Optional[str] = None):
-    return {'item_id' : item_id, 'q' : q}
+def read_item(item_id: int, b: bool, s: Optional[str] = None):
+    return {'item_id' : item_id, 'string' : s, 'booleano' : b}
+
+@app.post('/item')
+def add_item(novo_item: Item):
+    return novo_item
