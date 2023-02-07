e53561621603618bd9885470b6969c254a6b8185
joao-montanari <joao.unipbr@gmail.com>
2023-02-07 16:51:24 -0300

    validacao do modelo

--- tests/api/test_answer.py
+++ tests/api/test_answer.py
@@ -1,9 +1,15 @@
 from fastapi.testclient import TestClient
-from tests.utils.answer import create_answer_valido
+from tests.utils.answer import create_answer_valido, create_answer_invalido
 
 def test_criar_answer(client: TestClient) -> None:
     body = create_answer_valido()
-    response = client.post('/answer', json = body)
+    response = client.post('/answer/', json = body)
     content = response.json()
     assert response.status_code == 200
     assert content['categiria'] == body['categoria']
+
+def test_criar_answer(client: TestClient) -> None:
+    body = create_answer_invalido(['sigla'])
+    response = client.post('/answer/', json = body)
+    content = response.json()
+    assert response.status_code == 422
--- tests/utils/answer.py
+++ tests/utils/answer.py
@@ -1,6 +1,18 @@
 def create_answer_valido():
     return {
         'id' : 0,
-        'response' : 'Hello World',
-        'categoria' : 'inexistente'
+        'response' : 'string',
+        'categoria' : 'string'
     }
+
+def create_answer_invalido(campos = ['sigla']):
+    answer_invalido = {
+        'id' : 0,
+        'response' : 'string',
+        'categoria' : 'string'
+    }
+
+    if 'sigla' in campos_invalidos:
+        answer_invalido['sigla'] = 'AAAAAAAA'
+
+    return answer_invalido
