9c163e609ba3e190ce1ceeb21716c0b767d6e9d5
nychavs <chavesnaiury@gmail.com>
2023-03-10 16:49:29 -0300

    port updated

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+import uvicorn
 from rotas import router
 
 app = FastAPI()
@@ -9,3 +9,6 @@ def get_root():
     return {'mensagem': 'api de respostas bosch'}
 
 app.include_router(router, prefix="")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000, log_level="info")
